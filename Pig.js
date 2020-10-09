3ad16557e1c61799d3cbc8f0ea07e0641048d1b7
skardude12345 <souhardyakar11@gmail.com>
2020-10-09 21:29:51 +0900

    working on class 30

--- Pig.js
+++ Pig.js
@@ -2,6 +2,27 @@ class Pig extends BaseClass {
   constructor(x, y){
     super(x,y,50,50);
     this.image = loadImage("sprites/enemy.png");
+    this.transparency = 255;
   }
 
+  display(){
+    
+
+    if(this.body.speed > 4){
+
+      World.remove(world, this.body);
+
+      push();
+
+      this.transparency = this.transparency - 10;
+      tint(255, this.transparency);
+      image(this.image, this.body.position.x, this.body.position.y, 50, 50);
+
+      pop();
+
+    } else {
+      super.display();
+    }
+
+  }
 };
